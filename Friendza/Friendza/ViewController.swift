ffc1ffe4f73367ac6701078618e09d1c20849d91
jyoti shankar sahoo <jsahoo@apple.com>
2019-01-23 21:25:36 -0800

    Initial Commit

---
+++ Friendza/Friendza/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Friendza
+//
+//  Created by jyoti shankar sahoo on 1/23/19.
+//  Copyright © 2019 jyoti shankar sahoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
