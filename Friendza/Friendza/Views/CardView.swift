6c79e351d96bdfe2f12bafd8a2076e5f4f92a99f
jyoti shankar sahoo <jsahoo@apple.com>
2019-01-25 16:26:27 -0800

    Rotational Transformation and card dismiss

--- Friendza/Friendza/Views/CardView.swift
+++ Friendza/Friendza/Views/CardView.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class CardView: UIView {
     private let imageView = UIImageView(image: #imageLiteral(resourceName: "lady5c").withRenderingMode(.alwaysOriginal))
+    let panThreshold: CGFloat = 100
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -24,13 +25,9 @@ class CardView: UIView {
     @objc func handlePan(gesture: UIPanGestureRecognizer) {
         switch gesture.state {
         case .changed:
-            let translation = gesture.translation(in: nil)
-            self.transform = CGAffineTransform(translationX: translation.x, y: translation.y)
+            handleChanged(gesture)
         case .ended:
-            UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseInOut
-                , animations: {
-                    self.transform = .identity
-            }) { (_) in }
+            handleEnded(gesture)
         default:
             ()
         }
@@ -41,4 +38,28 @@ class CardView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
 
+    fileprivate func handleChanged(_ gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: nil)
+        let degree: CGFloat = translation.x / 20
+        let angle = degree * .pi / 180
+
+        let rotationalTransformation = CGAffineTransform(rotationAngle: angle)
+        self.transform = rotationalTransformation.translatedBy(x: translation.x, y: translation.y)
+    }
+
+    fileprivate func handleEnded(_ gesture: UIPanGestureRecognizer) {
+        let transalationDirection: CGFloat = gesture.translation(in: nil).x > 0 ? 1 : -1
+        let shouldDismissCard = abs(gesture.translation(in: nil).x) > panThreshold
+        UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseInOut
+            , animations: {
+                if shouldDismissCard {
+                    self.frame = CGRect(x: 1000 * transalationDirection, y: 0, width: self.superview!.frame.width, height: self.superview!.frame.height)
+                } else {
+                    self.transform = .identity
+                }
+        }) { (_) in
+            self.transform = .identity
+            self.frame = CGRect(x: 0, y: 0, width: self.superview!.frame.width, height: self.superview!.frame.height)
+        }
+    }
 }
